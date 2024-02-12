c3169d73a70f0210acb5337ba47ac1183139c3da
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-02-12 22:47:12 +0200

    floating-contact Finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgOptimizedImage, ViewportScroller } from '@angular/common';
+import { FloatingContactComponent } from '../floating-contact/floating-contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, NgOptimizedImage, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    NgOptimizedImage,
+    RouterLinkActive,
+    FloatingContactComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
