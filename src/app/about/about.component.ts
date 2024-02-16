f73d877e516f9bab2785e90c86036fe321821e7b
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-02-16 05:06:11 +0200

    Some progress in about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FloatingContactComponent } from '../floating-contact/floating-contact.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FloatingContactComponent, NgOptimizedImage],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
