3759888df12950ede6765add66839ec478561804
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-02-19 21:42:12 +0200

    Location page - Html structure

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -9,7 +9,7 @@ import { icon, latLng, marker, tileLayer } from 'leaflet';
   styleUrl: './locations.component.scss',
 })
 export class LocationsComponent {
-  options = {
+  canadaOptions = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -17,13 +17,13 @@ export class LocationsComponent {
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }),
     ],
-    zoom: 5,
-    center: latLng(-33.11336, 151.64716),
+    zoom: 13,
+    center: latLng(43.71705, -79.25469),
     zoomControl: false,
   };
 
-  layers = [
-    marker([-33.11336, 151.6471], {
+  canadaLayers = [
+    marker([43.71705, -79.25469], {
       icon: icon({
         iconSize: [25, 41],
         iconAnchor: [13, 41],
@@ -31,6 +31,56 @@ export class LocationsComponent {
         shadowUrl: 'assets/images/locations/desktop/marker-shadow.png',
       }),
       alt: 'Designo agency in Australia',
-    }).bindPopup(''),
+    }).bindPopup('1.Hey 🤨🤨🤨!!!!'),
+  ];
+
+  australiaOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }),
+    ],
+    zoom: 13,
+    center: latLng(-33.11336, 151.64716),
+    zoomControl: false,
+  };
+
+  australiaLayers = [
+    marker([-33.12575, 151.64042], {
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/images/locations/desktop/location-dot-solid.svg',
+        shadowUrl: 'assets/images/locations/desktop/marker-shadow.png',
+      }),
+      alt: 'Designo agency in Canada',
+    }).bindPopup('2.Do you want to play with me 😈😈😈?'),
+  ];
+
+  ukOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }),
+    ],
+    zoom: 13,
+    center: latLng(53.73373, -1.30021),
+    zoomControl: false,
+  };
+
+  ukLayers = [
+    marker([53.73373, -1.30021], {
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/images/locations/desktop/location-dot-solid.svg',
+        shadowUrl: 'assets/images/locations/desktop/marker-shadow.png',
+      }),
+      alt: 'Designo agency in UK',
+    }).bindPopup("3.I don't 😝😝😝"),
   ];
 }
