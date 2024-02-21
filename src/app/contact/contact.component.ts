20dc38f249987ea2dbfe4211091832ac3ad28b71
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-02-21 16:07:42 +0200

    using form control

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { LocationLinksComponent } from '../location-links/location-links.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [LocationLinksComponent],
+  imports: [LocationLinksComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  });
+  onSubmit() {
+    console.warn(this.contactForm.value);
+  }
+}
